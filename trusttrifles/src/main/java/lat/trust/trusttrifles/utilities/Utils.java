4c517c6214a4da70576f787633435f7a94db8697
fcaro2 <fcaro@jumpitt.com>
2019-03-31 11:37:06 -0300

    add network state

--- trusttrifles/src/main/java/lat/trust/trusttrifles/utilities/Utils.java
+++ trusttrifles/src/main/java/lat/trust/trusttrifles/utilities/Utils.java
@@ -242,4 +242,16 @@ public class Utils {
         }
 
     }
+
+    public static boolean chetNetworkState(Context context) {
+        boolean connected = false;
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+            //we are connected to a network
+            connected = true;
+        } else
+            connected = false;
+        return connected;
+    }
 }
