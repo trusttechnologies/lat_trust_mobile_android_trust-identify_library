ecd9cdb0c14c49b23bd8e0aa410948c6e165871a
fcaro2 <fcaro@jumpitt.com>
2019-04-04 11:08:32 -0300

    clear rest

--- trusttrifles/src/main/java/lat/trust/trusttrifles/utilities/SaveDeviceInfo.java
+++ trusttrifles/src/main/java/lat/trust/trusttrifles/utilities/SaveDeviceInfo.java
@@ -1,5 +1,10 @@
 package lat.trust.trusttrifles.utilities;
 
+import java.util.ArrayList;
+
+import lat.trust.trusttrifles.TrustClient;
+import lat.trust.trusttrifles.TrustListener;
+import lat.trust.trusttrifles.model.Audit;
 import lat.trust.trusttrifles.network.RestClientCompany;
 import lat.trust.trusttrifles.network.req.SaveDeviceInfoRequest;
 import retrofit2.Call;
@@ -19,14 +24,43 @@ public class SaveDeviceInfo {
             public void onResponse(Call<Void> call, Response<Void> response) {
                 if (response.isSuccessful()) {
                     TrustLogger.d("[TRUST CLIENT] DEVICE WAS SAVED CODE: " + response.code() + "DNI: " + dni + " BUNDLE ID: " + bundleId + " TRUST ID: " + trustId);
+                    TrustLogger.d("[TRUST ENROLLMENT] SENDING INFO TO TRUST ID...");
+
+                    getTrustId();
                 } else {
                     TrustLogger.d("[TRUST CLIENT] SAvING DEVICE CODE: " + response.code());
 
                 }
             }
+
             @Override
             public void onFailure(Call<Void> call, Throwable t) {
                 TrustLogger.d("[TRUST CLIENT] SAvING DEVICE ERROR: " + t.getMessage());
+            }
+        });
+    }
+
+    private static void getTrustId() {
+        TrustClient mClient = TrustClient.getInstance();
+        mClient.getTrifles(true, new TrustListener.OnResult<Audit>() {
+            @Override
+            public void onSuccess(int code, Audit data) {
+                TrustLogger.d("[TRUST ENROLLMENT] DATA SAVED FOR TRUST ID: " + data.getTrustid());
+            }
+
+            @Override
+            public void onError(int code) {
+
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+
+            }
+
+            @Override
+            public void onPermissionRequired(ArrayList<String> permisos) {
+
             }
         });
     }
