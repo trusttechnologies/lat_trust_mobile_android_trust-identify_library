3210abacb150a04f6b284831af7389f99dcdd7e5
fcaro2 <fcaro@jumpitt.com>
2019-05-10 14:06:28 -0400

    add ui

--- app/src/main/java/lat/trust/trustdemo/ui/audit/AuditActivity.java
+++ app/src/main/java/lat/trust/trustdemo/ui/audit/AuditActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import lat.trust.trustdemo.R;
+import lat.trust.trusttrifles.utilities.AutomaticAudit;
 
 public class AuditActivity extends AppCompatActivity {
 
@@ -11,5 +12,9 @@ public class AuditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audit);
+    }
+
+    private void sendAudit(){
+        AutomaticAudit.createAutomaticAudit("OPERACION","METODO" , "RESULT",AuditActivity.this);
     }
 }
--- app/src/main/java/lat/trust/trustdemo/ui/trustid/TrustIdActivity.java
+++ app/src/main/java/lat/trust/trustdemo/ui/trustid/TrustIdActivity.java
@@ -3,7 +3,12 @@ package lat.trust.trustdemo.ui.trustid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 import lat.trust.trustdemo.R;
+import lat.trust.trusttrifles.TrustClient;
+import lat.trust.trusttrifles.TrustListener;
+import lat.trust.trusttrifles.model.Audit;
 
 public class TrustIdActivity extends AppCompatActivity {
 
@@ -12,5 +17,29 @@ public class TrustIdActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_trust_id);
+    }
+
+    private void generateTrustId(){
+        TrustClient.getInstance().getTrifles(true, new TrustListener.OnResult<Audit>() {
+            @Override
+            public void onSuccess(int code, Audit data) {
+
+            }
+
+            @Override
+            public void onError(int code) {
+
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+
+            }
+
+            @Override
+            public void onPermissionRequired(ArrayList<String> permisos) {
+
+            }
+        });
     }
 }
