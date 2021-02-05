116bbffe224841edcef6825bb29943e1953c73e0
fcaro@trust.lat <44783089+fcaro2@users.noreply.github.com>
2021-02-05 12:21:10 -0300

    update lib and fix permission

--- trusttrifles/src/main/java/lat/trust/trusttrifles/managers/PermissionManager.java
+++ trusttrifles/src/main/java/lat/trust/trusttrifles/managers/PermissionManager.java
@@ -12,6 +12,7 @@ import com.orhanobut.hawk.Hawk;
 
 import lat.trust.trusttrifles.model.PermissionsGranted;
 import lat.trust.trusttrifles.utilities.Constants;
+import lat.trust.trusttrifles.utilities.TrustLogger;
 
 public class PermissionManager {
 
@@ -20,25 +21,45 @@ public class PermissionManager {
     }
 
     public static boolean isReadStorageGranted(Context context) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            int result = context.checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE);
-            return result == PackageManager.PERMISSION_GRANTED;
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                int result = context.checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE);
+                return result == PackageManager.PERMISSION_GRANTED;
+            }
+            return false;
+        } catch (Exception ex) {
+            TrustLogger.d("error in isReadStorageGranted: " + ex.getMessage());
+            return false;
         }
-        return false;
+
     }
 
     public static boolean isReadPhoneStateGranted(Context context) {
-        return context.checkSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED;
+        try {
+            return context.checkSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED;
+
+        } catch (Exception ex) {
+            TrustLogger.d("error in isReadPhoneStateGranted: " + ex.getMessage());
+            return false;
+        }
     }
 
     public static boolean isAccessCoarseLocationGranted(Context context) {
-        return context.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
-
+        try {
+            return context.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+        } catch (Exception ex) {
+            TrustLogger.d("error in isAccessCoarseLocationGranted: " + ex.getMessage());
+            return false;
+        }
     }
 
     public static boolean isAccessFineLocationGranted(Context context) {
-        return context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
-
+        try {
+            return context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+        } catch (Exception ex) {
+            TrustLogger.d("error in isAccessCoarseLocationGranted: " + ex.getMessage());
+            return false;
+        }
     }
 
     public static PermissionsGranted getPermissionGranted(Context context) {
